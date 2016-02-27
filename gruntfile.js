ccbe9fb758e3d3354bf66828c86d96f1d18b37f8
nikwhite <underscore.nik@gmail.com>
2016-02-27 00:21:50 -0600

    Add background around confirmation message

--- gruntfile.js
+++ gruntfile.js
@@ -82,7 +82,7 @@ module.exports = function(grunt) {
 		connect: {
 			server: {
 				options: {
-					port: 8080,
+					port: 9000,
 					base: 'dist',
 					hostname: '0.0.0.0',
 					logger: 'dev',
