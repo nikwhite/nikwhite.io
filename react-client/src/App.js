d055136027fd7dc1464707081c38d0acd338cc7a
Nik White <underscore.nik@gmail.com>
2022-01-23 15:36:01 -0600

    Add connect4, implemented as a graph

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import TicTacToe from './games/tictactoe'
+import Connect4 from './games/connect4'
 
 function App() {
   return (
@@ -16,19 +17,23 @@ function App() {
           early 2000s, when I was a kid, making forums and blogs for my gaming 
           groups. I started building websites and office networks for small businesses 
           and individuals in high school. After studying computer science and art at 
-          Northern Illinoi University I spent 8 years at JPMorgan Chase building things on 
+          Northern Illinois University I spent 8 years at JPMorgan Chase building things on 
           the internet. First websites, then I led development on a design
-          system project for redesigning and replatforming the chase.com CMS. I then led development
-          on a payment platform for 4 years. Then I moved to the Chase Online
-          Core Platform to build shared tools, patterns, and APIs. 
+          system project to redesign and replatform the chase.com website and CMS. I then led development
+          on a payment platform for 4 years from prototype, to initial merchants, and finally mass rollout.
+          Most recently, I joined Chase Online Core Platform to build shared tools, patterns, and APIs. 
           I also provided support and mentorship to over 500 engineers building 
-          apps on the platform. I left in mid-2020 and I'm now actively looking for new oppotunities.
+          apps on the platform. I left in mid-2020 and I'm now actively looking for new opportunities.
         </p>
       </section>
       <section className="container">
         <h2>Games</h2>
-        <h3>TicTacToe</h3>
-        <TicTacToe />
+        <div className="game">
+          <TicTacToe />
+        </div>
+        <div className="game">
+          <Connect4 />
+        </div>
       </section>
     </div>
   );
---
+++ react-client/src/games/connect4.js
@@ -0,0 +1,174 @@
+import React, {useState} from 'react'
+import './connect4.css'
+
+function BoardNode(row, col) {
+    this.row = row
+    this.col = col
+    this.color = ''
+    this.sameColorNeighbors = {
+        // this way, +/- 4 results in a straight line
+        // when position < 4, compliment = position + 4
+        // when position >= 4, compliment = position - 4
+        0: null, 1: null, 2: null, 
+        7: null, /*node*/ 3: null, 
+        6: null, 5: null, 4: null
+  }
+}
+
+const HEIGHT = 6
+const WIDTH = 7
+const PINK = 'pink'
+const BLUE = 'blue'
+const OPPOSITE_NODES = [
+  [7,3], // row
+  [1,5], // col
+  [2,6], // diag
+  [0,4], // diag
+]
+
+function gameBoardFactory() {
+  let emptyGameBoard = []
+  for (let i=0; i < HEIGHT; i++) {
+    let row = []
+    for (let j=0; j < WIDTH; j++) {
+      row[j] = new BoardNode(i, j)
+    }
+    emptyGameBoard[i] = row
+  }
+  return emptyGameBoard
+}
+
+const EMPTY_STARTING_BOARD = gameBoardFactory()
+
+function getLowestEmptyNode(board, col) {
+  let row = board.length - 1
+  //bottom up simpler than top-down and back-track
+  while (row >= 0) {
+    if (!board[row][col].color) {
+      return board[row][col]
+    }
+    row--
+  }
+  return null
+}
+
+function getAdjacentNodes(board, node) {
+  let prevRow = board[node.row - 1]
+  let curRow = board[node.row]
+  let nextRow = board[node.row + 1]
+
+  prevRow = prevRow ? 
+    [prevRow[node.col-1], prevRow[node.col], prevRow[node.col+1]]
+    : []
+  curRow = [curRow[node.col-1], curRow[node.col+1]]
+  nextRow = nextRow ? 
+    [nextRow[node.col-1], nextRow[node.col], nextRow[node.col+1]]
+    : []
+
+  return {
+    0: prevRow[0], 1: prevRow[1], 2: prevRow[2],
+    7: curRow[0],   /**node **/   3: curRow[1],
+    6: nextRow[0], 5: nextRow[1], 4: nextRow[2]
+  }
+}
+
+function filterSameColor(color, adjacentNodes) {
+  return Object.entries(adjacentNodes).reduce((filtered, [position, node]) => {
+    if (node && node.color === color) {
+      filtered[position] = node
+    }
+    return filtered
+  }, {})
+}
+
+function setAdjacentSameColorNeighbors(node) {
+  Object.entries(node.sameColorNeighbors)
+        .forEach(([position, neighbor]) => {
+          if (neighbor && neighbor.color === node.color) {
+            position = Number(position)
+            let complimentPosition = position + (position < 4 ? 4 : -4)
+            neighbor.sameColorNeighbors[complimentPosition] = node
+          } 
+        })
+}
+
+function getNodesFromPosition(node, position, carry) {
+  carry = carry || []
+  if (node.sameColorNeighbors[position]) {
+    let nextNode = node.sameColorNeighbors[position]
+    carry.push(nextNode)
+    return getNodesFromPosition(nextNode, position, carry)
+  }
+  return carry
+}
+
+function getWinners(node) {
+  return OPPOSITE_NODES.reduce((winners, [left, right]) => {
+    if (node.sameColorNeighbors[left] || node.sameColorNeighbors[right]) {
+      let chain = [
+        ...getNodesFromPosition(node, left),
+        node,
+        ...getNodesFromPosition(node, right)
+      ]
+      if (chain.length >= 4) {
+        winners.push(chain)
+      }
+    }
+    return winners
+  }, [])
+}
+
+function Connect4() {
+  const [board, setBoard] = useState(EMPTY_STARTING_BOARD)
+  const [turn, setTurn] = useState(PINK)
+  const [winners, setWinners] = useState([])
+
+  function handleClick(column){
+    if (winners.length) return
+
+    let node = getLowestEmptyNode(board, column)
+
+    if (node && !node.color) {
+      node.color = turn
+      
+      let adjacentNodes = getAdjacentNodes(board, node)
+      node.sameColorNeighbors = {
+        ...node.sameColorNeighbors,
+        ...filterSameColor(node.color, adjacentNodes)
+      }
+
+      setAdjacentSameColorNeighbors(node)
+      setBoard(board)
+      setTurn(turn === PINK ? BLUE : PINK)
+      setWinners( getWinners(node) )
+    }
+  }
+
+  if (winners) {
+    winners.forEach(winnerChain => {
+      winnerChain.forEach(node => node.winner = true)
+    })
+  }
+  
+  return (
+    <div className="connect4board">
+      <h3>Connect4</h3>
+      {board.map((row, i) => (
+        <div className="row" key={'c4,'+i}>
+          {row.map((node, col) => (
+            <div 
+              key={[i,col].join()}
+              onClick={e => handleClick(col)}
+              className={'slot '+(node.winner?'winner':'')}>
+              {node.color && 
+                <div className={node.color +'-token'}></div>
+              }
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Connect4
