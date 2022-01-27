f502c00d2b820562d1d9ade97445bf14376a70f3
Nik White <underscore.nik@gmail.com>
2022-01-27 12:03:42 -0600

    Adding a pic

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -3,18 +3,20 @@ import TicTacToe from './games/tictactoe'
 import Connect4 from './games/connect4'
 import IconLink from './components/iconLink'
 import DocIcon from './components/docIcon'
+import portrait from './img/nik250.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header container">
+        <img src={portrait} alt="Portrait" />
         <h1>Nik White<br />
           <span>Software Engineer, Polymath, Autodidact, INTP</span>
         </h1>
       </header>
       <section className="container">
         <p>
-          I've been a web engineer building things on the internet since i was a kid in the 
+          I've been a web engineer building things on the internet since I was a kid in the 
           early 2000s making forums and blogs for my gaming groups.
           I started building websites and office networks for small businesses 
           and individuals in high school. After studying computer science and art at 
@@ -24,7 +26,7 @@ function App() {
           on a payment platform for 4 years from prototype, to initial merchants, and finally mass rollout.
           Most recently, I joined Chase Online Core Javascript Platform to build shared tools, patterns, and APIs. 
           I also provided support and mentorship to over 500 engineers building 
-          apps on the platform. I left in mid-2020 and I'm now actively looking for new opportunities.
+          apps on the platform. I left in mid-2020 and I'm now actively looking for a new challenge.
         </p>
         <p>
           <a href="https://docs.google.com/document/d/e/2PACX-1vQ38zIjQZFxu1urpmR6HCwuRCa_DRHpKbgAAge2KRuiDrmn_MjbRwEJ57QzUWtTvPvihvLf7EKO94T9/pub"
